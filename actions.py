c4616b32decdca40b90282107178fa5f77b3ce2e
smkmth <dandavidnoon@gmail.com>
2016-04-20 09:46:32 +0100

    Added files via upload

---
+++ actions.py
@@ -0,0 +1,12 @@
+import Player
+
+class Action():
+    def __init__(self, method, name, hotkey, **kwargs):
+        self.method = method
+        self.hotkey = hotkey
+        self.name = name
+        self.kwargs = kwargs
+
+    def __str__(self):
+        return "{}: {}".format(self.hotkey, self.name)
+    
